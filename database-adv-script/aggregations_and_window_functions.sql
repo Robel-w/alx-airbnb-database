2967044dc6a8dcc9816d415ca2a2242c7cb4ab89
Robel Wondwosen <121330659+Robel-w@users.noreply.github.com>
2025-05-16 17:44:18 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -6,10 +6,18 @@ FROM bookings
 GROUP BY user_id
 ORDER BY total_bookings DESC;
 
---Rank properties by total number of Bookings
+-- Rank properties by total number of Bookings
 SELECT 
     property_id,
     COUNT(*) AS total_bookings,
     ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS rank
+FROM bookings
+GROUP BY property_id;
+
+-- Rank properties using RANK() to handle ties
+SELECT 
+    property_id,
+    COUNT(*) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS rank_with_ties
 FROM bookings
 GROUP BY property_id;
