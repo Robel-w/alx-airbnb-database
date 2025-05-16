7d3607d8b8efb3746143a7ba704979542cfd6e35
Robel Wondwosen <121330659+Robel-w@users.noreply.github.com>
2025-05-16 17:20:02 +0300

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,20 @@
+--AVG > 4.0 rating
+SELECT *
+FROM properties
+WHERE id IN (
+    SELECT property_id
+    FROM reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+)
+ORDER BY id;
 
+-- >3 bookings
+SELECT * 
+FROM users
+WHERE(
+    SELECT COUNT(*)
+    FROM bookings
+    WHERE booking.user_id = users.id
+) > 3
+ORDER BY users.id;
